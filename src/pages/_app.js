290777139086004a750d8a11c9b04d3dd1af14fd
jay75chauhan <chauhan75jayk@gmail.com>
2021-08-06 20:47:02 +0530

    progress br added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,20 @@ import { Provider as AuthProvider } from "next-auth/client";
 
 import "../styles/globals.css";
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FBBF24",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <AuthProvider session={pageProps.session}>
